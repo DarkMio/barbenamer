771d9f4be50bfd10c8936b02a2a54b990b12fc37
Martin Zier <DarkMio@users.noreply.github.com>
2024-08-04 21:29:55 +0200

    fix: more social fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/barbenamer/',
   build: {
     assetsInlineLimit: 0
   },
